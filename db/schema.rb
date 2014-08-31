c8fc5ce3bf563a5583f00bd64302a26d802e9dd0
Joel Lusky <joelzlusky@gamil.com>
2014-08-31 00:36:42 -0400

    Removed url from bill table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829183930) do
+ActiveRecord::Schema.define(version: 20140831034612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20140829183930) do
   create_table "bills", force: true do |t|
     t.string   "name"
     t.integer  "category_id"
-    t.string   "url"
     t.date     "duedate"
     t.boolean  "paid"
     t.datetime "created_at"
