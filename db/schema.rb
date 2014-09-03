6ad63570623566b58722e3a1ca6488119a83c141
alfonsopintos <alfonsopintos@gmail.com>
2014-09-03 13:31:25 -0400

    modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -56,12 +56,12 @@ ActiveRecord::Schema.define(version: 20140902231354) do
   add_index "reminders", ["bill_id"], name: "index_reminders_on_bill_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(version: 20140902231354) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "cell_phone"
-    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
