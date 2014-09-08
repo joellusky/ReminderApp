f7e08e53d90254e8348655d33b372b3ad3c729a9
Bayardo Navarro <bayardo@me.com>
2014-09-08 18:05:54 -0400

    removed weather widget

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,28 +14,4 @@
 //= require jquery_ujs
 //= require jquery-ui/datepicker
 //= require twitter/bootstrap
-//= require_tree .
-
-var ready
-
-var ready = function(){
-  $.simpleWeather({
-    location: 'Miami, FL',
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      // html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      // html += '<li class="currently">'+weather.currently+'</li>';
-      // html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-  
-      $("#weather").html(html);
-    },
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-  });
-};
-
-
-$(document).on('ready page:ready', ready);
+//= require_tree 
