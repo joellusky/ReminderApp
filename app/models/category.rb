aefe3f4abe5f76611a9c6d2a430c37eee3d91ca3
alfonsopintos <alfonsopintos@gmail.com>
2014-09-30 13:01:57 -0400

    added catgeory tests and model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
 	has_many :providers
+	validates :name, presence: true
 end
--- spec/units/create_category_spec.rb
+++ spec/units/create_category_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 describe Category do
 
-	it "is named FPL" do
+	it "should have a name" do
 		category = Category.new
 		category.name = "FPL"
 		category.save
 		expect(category.name).to eq("FPL")
 		expect(Category.count).to eq(1)
 	end
+
+	it "should not save when no name provided" do
+		category = Category.new
+		category.name = ""
+		category.save
+		expect(Category.count).to eq(0)
+	end
+
 
 end
