9be9c867fc26b9ae4104d9f83d44f8467ff71950
Joel Lusky <joelzlusky@gmail.com>
2014-10-09 18:23:56 -0400

    sidekiq delete and update workers

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -70,125 +70,19 @@ class Bill < ActiveRecord::Base
     self.event_recurrence.dates(options)
   end
 
-  # def send_recurrence
-  #   # SendWorker.perform(self.id)
-  #   # SendWorker.perform_async(@bill.id)
-  #   # if self.contact_method == 'text'
-    
-  #   # HTTParty.post("http://localhost:8080/texts.json", 
-  #   #     :body => {
-  #   #       'event_recurrence' => {
-  #   #         'object_id' => self.id,
-  #   #         'end_date' => 1.year.from_now,
-  #   #         'every' => self.every,
-  #   #         'start_date' => self.duedate}, 
-  #   #       'text' => { 
-  #   #         'cell_phone' => self.user.cell_phone,
-  #   #         'text_reminder' => "This is a reminder that your #{self.provider.name} bill is due tomorrow. #{self.provider.url}"
-  #   #       }
-  #   #        }.to_json, 
-        
-  #   #     :headers => { 
-  #   #       'Authorization' => "Token token=#{@@token}",
-  #   #       'Content-Type' => 'application/json',
-  #   #       'Accept' => "application/json" } )
-   
-  #   # elsif self.contact_method == 'phone call'
-
-  #   #   HTTParty.post("http://localhost:8080/calls.json", 
-  #   #     :body => {
-  #   #       'event_recurrence' => {
-  #   #         'object_id' => self.id,
-  #   #         'end_date' => 1.year.from_now,
-  #   #         'every' => self.every,
-  #   #         'start_date' => self.duedate}, 
-  #   #       'call' => { 
-  #   #         'cell_phone' => self.user.cell_phone,
-  #   #         'call_reminder' => "Hello #{self.user.first_name}. This is a friendly reminder that your #{self.provider.name}, #{self.category.name} bill is due tomorrow. Thank you for using Forget Me Not. GoodBye!"
-  #   #       }
-  #   #        }.to_json, 
-        
-  #   #     :headers => { 
-  #   #       'Authorization' => "Token token=#{@@token}",
-  #   #       'Content-Type' => 'application/json',
-  #   #       'Accept' => "application/json" } )
-
-  #   # elsif self.contact_method == 'email'
-
-  #   #   HTTParty.post("http://localhost:8080/emails.json", 
-  #   #     :body => {
-  #   #       'event_recurrence' => {
-  #   #         'object_id' => self.id,
-  #   #         'end_date' => 1.year.from_now,
-  #   #         'every' => self.every,
-  #   #         'start_date' => self.duedate}, 
-  #   #       'email' => { 
-  #   #         'email_address' => self.user.email,
-  #   #         'email_reminder' => "Hello #{self.user.first_name}. This is a friendly reminder that your #{self.provider.name}, #{self.category.name} bill is due tomorrow. Thank you for using Forget Me Not"
-
-  #   #       }
-  #   #        }.to_json, 
-        
-  #   #     :headers => { 
-  #   #       'Authorization' => "Token token=#{@@token}",
-  #   #       'Content-Type' => 'application/json',
-  #   #       'Accept' => "application/json" } )
-  #   # end
-  # end
-
   def delete_recurrence
-    response = HTTParty.get("http://localhost:8080/event_recurrences.json")
-    # a is an array of hashes. each hash being an event recurrence. 
-    a = JSON.parse(response.body)
-    # a.each iterates over every hash in the response array.
-    a.each do |hash|
-      # if the hash has a key 'bill_id' whos value is equal to that of self.id
-      if hash['object_id'] == self.id.to_s
-        # @match represents the hash of the event recurrence that needs to be deleted on reminderService, when a bill is deleted in ReminderApp
-        @match = hash
-      end
-    end
-  
-
-    #initiaes a delete request and interpolates the value of key[ID], and does converts it to integer.
-    HTTParty.delete("http://localhost:8080/event_recurrences/#{@match['id'].to_i}.json", 
-    :body => {'object_id' => self.id }.to_json, 
-
-    :headers => { 
-          'Authorization' => "Token token=#{@@token}",
-          'Content-Type' => 'application/json',
-          'Accept' => "application/json" } )
+    DeleteWorker.perform_async(self.id)
   end
 
   def update_recurrence
-    
-    response = HTTParty.get("http://localhost:8080/event_recurrences.json")
-    a = JSON.parse(response.body)
-    a.each do |hash|
-      if hash['object_id'] == self.id.to_s
-        @match = hash
-      end
-    end
-    
-    HTTParty.patch("http://localhost:8080/event_recurrences/#{@match['id'].to_i}.json", 
-    :body => {'object_id' => self.id,
-    'end_date' => self.duedate + 1.year,
-    'every' => self.every,
-    'start_date' => self.duedate, 
-    'cell_phone' => self.user.cell_phone,
-    'email' => self.user.email }.to_json, 
-
-    :headers => { 
-          'Authorization' => "Token token=#{@@token}",
-          'Content-Type' => 'application/json',
-          'Accept' => "application/json" } )
+    UpdateWorker.perform_async(self.id)
   end
 
   #if a bill is updated with a new contact method, 
   #this method will delete the old instance and add a new one with the new contact 
   def update_contact_method
-      delete_recurrence
-      send_recurrence
+    DeleteWorker.perform_async(self.id)
+    SendWorker.perform_async(self.id)
   end
 end
 
