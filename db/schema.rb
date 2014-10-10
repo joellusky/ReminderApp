26bfcd19886de1d98bb39e336e1e447b192addb9
Bayardo Navarro <bayardo@me.com>
2014-10-10 16:01:34 -0400

    design update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930142351) do
+ActiveRecord::Schema.define(version: 20141007215138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,10 @@ ActiveRecord::Schema.define(version: 20140930142351) do
     t.boolean  "admin"
     t.string   "avatar"
     t.string   "country"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
