769065ba208ecfae1216eb8ffe0872103fa694e7
Joel Lusky <joelzlusky@gmail.com>
2014-10-10 19:59:59 -0400

    fixed delete recurrence bug

--- app/workers/delete_worker.rb
+++ app/workers/delete_worker.rb
@@ -4,10 +4,10 @@ include Sidekiq::Worker
 sidekiq_options retry: false
 
 
-@@token = "00fd8690272c3a53aa4ae8527b68b18d"
+@@token = "d26f11c26ff5472f884e864f2d232f08"
 
   def perform(bill_id)
-    bill = Bill.find(bill_id)
+    # bill = Bill.find(bill_id)
 
     response = HTTParty.get("http://localhost:8080/event_recurrences.json")
     # a is an array of hashes. each hash being an event recurrence. 
@@ -15,7 +15,7 @@ sidekiq_options retry: false
     # a.each iterates over every hash in the response array.
       a.each do |hash|
     # if the hash has a key 'object_id' whos value is equal to that of bill.id
-        if hash['object_id'] == bill.id.to_s
+        if hash['object_id'] == bill_id.to_s
     # @match represents the instance on the API that needs to be deleted, when a bill is deleted in ReminderApp
           @match = hash
         end
@@ -24,7 +24,7 @@ sidekiq_options retry: false
 
     #Does the delete by inputting the instance ID of the object we are attempting to delete
     HTTParty.delete("http://localhost:8080/event_recurrences/#{@match['id'].to_i}.json", 
-    :body => {'object_id' => bill.id }.to_json, 
+    :body => {'object_id' => bill_id }.to_json, 
 
     :headers => { 
           'Authorization' => "Token token=#{@@token}",
