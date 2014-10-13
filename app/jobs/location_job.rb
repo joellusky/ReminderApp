385569e824eb9d886e936a7aa86e674014f0ba8e
alfonsopintos <alfonsopintos@gmail.com>
2014-10-12 21:57:12 -0400

    switched net impact for free geo ip. More hits per hour available.

--- app/jobs/location_job.rb
+++ app/jobs/location_job.rb
@@ -1,20 +1,15 @@
 class LocationJob
 
-include SuckerPunch::Job
+  include SuckerPunch::Job
 
   def perform(user_id)
 
     user = User.find(user_id)
-
     ip = user.last_sign_in_ip
-    response = Unirest.get "http://api.netimpact.com/qv1.php?key=yvnBK1wYiEwrmCHC&qt=geoip&d=json&q=#{ip}&qt=geoip",
-    headers:{:X => "L90l5rQA7smshIUnQLnW4YYUc3kzp1VZhEZjsnUq2OaaQJwhol"}
-
-    location = response.body
-
-    user.country = location[0][2]
+    response = HTTParty.get("http://www.freegeoip.net/json/#{ip}")
+    a = JSON.parse(response.body)
+    user.country = a["country_name"]
     user.save
-
   end
 
 end
