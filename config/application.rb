ec5b8863f85d5b0688ad5cf6981557fe76a860ad
alfonsopintos <alfonsopintos@gmail.com>
2014-10-15 11:08:26 -0400

    time zone to eastern

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# config.time_zone = 'Eastern Time'
+config.time_zone = 'Eastern Time'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
