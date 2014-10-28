3c36fb82560c25029ccfdfd85aeb3ab6ff9373e7
Joel Lusky <joelzlusky@gmail.com>
2014-10-28 16:48:29 -0400

    pages callbacks

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,7 @@
 class Page < ActiveRecord::Base
+    # add callbacks to write changes to git like this:
+  after_create  :create_page
+  after_update  :update_page
+  after_destroy :delete_page
 end
+
